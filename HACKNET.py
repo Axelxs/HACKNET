8d24eeadaba9e837848cece487a78670ec9aee45
Axelxs <88009984+Axelxs@users.noreply.github.com>
2022-09-27 17:50:50 -0500

    Create HACKNET.py

---
+++ HACKNET.py
@@ -0,0 +1,341 @@
+import os
+os.system("clear")
+import sys
+import time
+from time import sleep
+import random
+import requests
+a = random.randrange(1,16)
+if a == 1:
+    a="197.158.100.9"
+elif a == 2:
+    a="196.168.100.195"   
+elif a == 3:
+    a="198.178.160.80"
+elif a == 4:
+    a="196.126.100.70"
+elif a == 5:
+    a="193.156.120.35"
+elif a == 6:
+    a="192.100.179.10" 
+elif a == 7:
+    a="198.167.168.80"
+elif a == 8:
+    a="195.132.100.70" 
+elif a == 9:
+    a="194.120.100.40"
+elif a == 10:
+    a="196.120.150.10"  
+elif a == 11:
+    a="194.102.130.18"
+elif a == 12:
+    a="196.120.178.15"
+elif a == 13:
+    a="196.129.168.17"
+elif a == 14:
+    a="195.120.100.16" 
+elif a == 15:
+    a="198.167.178.30"
+elif a == 16:
+    a="194.132.100.70"   
+
+m = random.randrange(1,4)    
+if m == 1:
+    m = 10
+if m == 2:
+    m = 8
+if m == 3:
+    m = 12
+if m == 4:
+    m = 7 
+def p():
+    i = random.randrange(1,8)
+    if i == 1:
+        print("*****801")
+    if i == 2:    
+        print("******31")
+    if i == 3:
+        print("*****bc")   
+    if i == 4:
+        print("*****8J0")  
+    if i == 5:
+        print("*****81")
+    if i == 6:    
+        print("******1")
+    if i == 7:
+        print("*****89")
+    if i == 8:
+        print("*****123" )
+def l():  
+    o = random.randrange(1,4) 
+    if o == 1:
+        print("+********102")      
+    if o == 2:
+        print("+********710")
+    if o == 3:
+        print("+********537")   
+    if o == 4:
+        print("+********589")
+    if o == 5:
+        print("+********812")   
+    if o == 6:
+        print("+********591")  
+    if o == 7:
+        print("+********330")    
+    if o == 8:
+        print("+********430")    
+    if o == 9:
+        print("+********540")  
+    if o == 10:
+        print("+********503")
+def q():  
+    o = random.randrange(1,4) 
+    if o == 1:
+        print("***102")      
+    if o == 2:
+        print("****10")
+    if o == 3:
+        print("***457")   
+    if o == 4:
+        print("***419")
+    if o == 5:
+        print("***781")   
+    if o == 6:
+        print("***301")  
+    if o == 7:
+        print("***910")    
+    if o == 8:
+        print("***341")    
+    if o == 9:
+        print("***671")               
+RR = "\033[32;m"
+x = "\033[31;m"
+h ="\033[34;m"
+u = "\033[36;m"
+r = "\033[33;m"
+def f(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(1./170)
+00
+def e(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(6./50)
+00
+f(RR+"█░░█ █▀▀█ █▀▀ █░█ █▀▀▄ █▀▀ ▀▀█▀▀")
+f(RR+"█▀▀█ █▄▄█ █░░ █▀▄ █░░█ █▀▀ ░░█░░")
+f(RR+"▀░░▀ ▀░░▀ ▀▀▀ ▀░▀ ▀░░▀ ▀▀▀ ░░▀░░")
+b = int(input("1.-TIKTOK\n2.-INSTAGRAM\n3.-FACEBOOK\n4.-Telegram\n5.-WATSAPP\n>>>"))
+if b == 1:
+
+    c = input("Username: ")
+    c = f"@{c}"   
+    d = f"Buscando claves de {c} ..."
+    e(RR+d)
+    sleep(2)
+    e(RR+"Buscando IP's...")
+    sleep(1)
+    j = 32895
+    for g in range (1,m):
+            print(h+"127.0.0.1:", j)
+            j=j+7412
+            sleep(1)
+    f(u+ str(a))
+    sleep(1)
+    print(u, "SUCCESSFUL")
+    e(RR+f"Buscando servidores a activos de {a}...")
+    print(r+"")
+    g = 9873
+    for sa in range(1,m):
+        sleep(1)
+        print(r+a,"::",g)
+        g=g+4172 
+    g=g+1291     
+    e(u+f"{a}::{g}")  
+    e(u+"SUCCESSFUL")     
+    e(RR+'CRACKING FOR PASSWORD...')
+    print(r+"")
+    for y in range(1,m):
+        sleep(1)
+        p()
+    e(u+"password successful, packing in password.txt")
+    e(RR+"information for database...")
+    e(RR+"MySQL and SQLMAP successful")
+    e(u+"databases successful, packing in databases.txt")   
+    e(u+'successful')
+if b == 2:
+
+    c = input("Username: ")
+    c = f"@{c}"   
+    d = f"Buscando claves de {c} ..."
+    e(RR+d)
+    sleep(2)
+    e(RR+"Buscando IP's...")
+    sleep(1)
+    e(RR+"Entrando a la base de datos")
+    j = 15895
+    for g in range (1,m):
+            print(h+"127.0.0.1:", j)
+            j=j+8567
+            sleep(1)
+    f(u+ str(a))
+    sleep(1)
+    print(u, "SUCCESSFUL")
+    e(RR+f"Buscando servidores a activos de {a}...")
+    print(r+"")
+    g = 9873
+    for sa in range(1,m):
+        sleep(1)
+        print(r+a,"::",g)
+        g=g+4172 
+    g=g+1291     
+    e(u+f"{a}::{g}")  
+    e(u+"SUCCESSFUL")     
+    e(RR+'CRACKING FOR PASSWORD...')
+    print(r+"")
+    for y in range(1,m):
+        sleep(1)
+        p()
+        
+    e(u+"password successful, packing in password.txt(2)")
+    e(RR+"information for database...")
+    e(RR+"information successful")
+    e(u+"databases successful, packing in databases.txt(2)")   
+    e(u+'successful')    
+    
+if b == 3:
+
+    c = input("Username: ")
+    c = f"@{c}"   
+    d = f"Buscando claves de {c} ..."
+    e(RR+d)
+    sleep(2)
+    e(RR+"Entrando a 'META'...")
+    sleep(1)
+    e(RR+"Buscando IP's...")
+    sleep(1)
+    e(RR+"Entrando a la base de datos de 'META' ")
+    sleep(1)
+    j = 18102
+    for yu in range (1,m):
+            print(h+"127.0.0.1::", j)
+            j=j+7120
+            sleep(1)
+    f(u+ str(a))
+    sleep(1)
+    e(RR+f"Buscando servidores a activos de {a}...")
+    print(r+"")
+    g = 9873
+    for sa in range(1,m):
+        sleep(1)
+        print(r+a,"::",g)
+        g=g+4172 
+    g=g+1291     
+    e(u+f"{a}::{g}")  
+    e(u+"SUCCESSFUL")     
+    print(u, "SUCCESSFUL")
+    e(RR+'CRACKING FOR PASSWORD...')
+    print(r+"")
+    for y in range(1,m):
+        sleep(1)
+        p()
+        
+    e(u+"password successful, packing in password.txt(3)")
+    e(RR+"information for database...")
+    e(RR+"information successful")
+    e(u+"databases successful, packing in databases.txt(3)")   
+    e(u+'successful') 
+if b == 4:
+
+    c = input("Username: ")
+    c = f"@{c}"   
+    d = f"Buscando claves de {c} ..."
+    e(RR+d)
+    sleep(2)
+    e(RR+"Buscando IP's...")
+    sleep(1)
+    e(RR+"Entrando a la base de datos")
+    j = 29170
+    for g in range (1,m):
+            print(h+"127.0.0.1:", j)
+            j=j+5401
+            sleep(1)
+    f(u+ str(a))
+    sleep(1)
+    print(u, "SUCCESSFUL")
+    e(RR+'CRACKING FOR FILES')
+    e(u+"files successful, packing in files.txt(4)")
+    e(RR+"Check for numer")
+    sleep(1)
+    print(r+"")
+    for y in range(1,m):
+        sleep(1)
+        l()    
+    e(u+"numer successful, packing in  numer.txt(4)")
+    sleep(1)
+    e(RR+"Creando copia de segiridad...")
+    e(RR+"EXPORTANDO COPIA...")
+    e(u+"successful")
+    e(RR+f"Buscando servidores a activos de {a}...")
+    print(r+"")
+    g = 9873
+    for sa in range(1,m):
+        sleep(1)
+        print(r+a,"::",g)
+        g=g+4172 
+    g=g+1291     
+    e(u+f"{a}::{g}")  
+    e(u+"SUCCESSFUL") 
+    e(RR+"information for database...")
+    e(RR+"information successful")
+    e(u+"databases successful, packing in databases.txt(4)")   
+    e(u+'successful')  
+        
+if b == 5:
+
+    c = input("numero: ")
+    c = f"@{c}"   
+    d = f"Buscando claves de {c} ..."
+    e(RR+d)
+    sleep(2)
+    e(RR+"Buscando IP's...")
+    sleep(1)
+    e(RR+"Entrando a la base de datos")
+    j = 29170
+    for g in range (1,m):
+            print(h+"127.0.0.1:", j)
+            j=j+5401
+            sleep(1)
+    f(u+ str(a))
+    sleep(1)
+    print(u, "SUCCESSFUL")
+    e(RR+'CRACKING FOR FILES')
+    e(u+"files successful, packing in files.txt(4)")
+    e(RR+"Check for code verify")
+    sleep(1)
+    print(r+"")
+    for y in range(1,m):
+        sleep(1)
+        q()    
+    e(u+"code successful, packing in  code.txt(4)")
+    sleep(1)
+    e(RR+"Creando copia de segiridad...")
+    e(RR+"EXPORTANDO COPIA...")
+    e(u+"successful")
+    e(RR+f"Buscando servidores a activos de {a}...")
+    print(r+"")
+    g = 9873
+    for sa in range(1,m):
+        sleep(1)
+        print(r+a,"::",g)
+        g=g+4172 
+    g=g+1291     
+    e(u+f"{a}::{g}")  
+    e(u+"SUCCESSFUL") 
+    e(RR+"information for database...")
+    e(RR+"information successful")
+    e(u+"databases successful, packing in databases.txt(4)")   
+    e(u+'successful')                                
